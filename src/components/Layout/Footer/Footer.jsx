5852fc35a6420dfc444e29c6ece3b339a5a26a10
Szymon Czapkowski <szymon.czapkowski@gmail.com>
2019-05-07 00:39:59 +0200

    Footer and Header

--- src/components/Layout/Footer/Footer.jsx
+++ src/components/Layout/Footer/Footer.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
 import { FooterContainer } from './FooterStyles';
+import { FindItLogo } from './FooterStyles';
+import { GithubLogo } from './FooterStyles';
+import gitLogo from '../../../images/github_logo.png';
+import findItLogo from '../../../images/findit_logo.png';
+
 
 const Footer = () => {
 
         return (
             <FooterContainer>
-                Footer
+                <FindItLogo>
+                    <img src={findItLogo} alt="logo" />
+                </FindItLogo>
+                <GithubLogo>
+                    <a href="https://github.com/SzymonCzapkowski" title="Szymon Czapkowski"><img src={gitLogo} alt="logo" /></a>
+                    <a href="https://github.com/SzymonCzapkowski" title="Szymon Czapkowski"><img src={gitLogo} alt="logo" /></a>
+                    <a href="https://github.com/SzymonCzapkowski" title="Szymon Czapkowski"><img src={gitLogo} alt="logo" /></a>
+                    <a href="https://github.com/SzymonCzapkowski" title="Szymon Czapkowski"><img src={gitLogo} alt="logo" /></a>
+                    <a href="https://github.com/SzymonCzapkowski" title="Szymon Czapkowski"><img src={gitLogo} alt="logo" /></a>
+                    <a href="https://github.com/SzymonCzapkowski" title="Szymon Czapkowski"><img src={gitLogo} alt="logo" /></a>
+                </GithubLogo>
             </FooterContainer>
         );
 
--- src/components/Layout/Footer/FooterStyles.js
+++ src/components/Layout/Footer/FooterStyles.js
@@ -1,6 +1,27 @@
 import styled from 'styled-components';
 
 export const FooterContainer = styled.div`
-    color: red;
-    font-size: 30px;
+    display: grid;
+    grid-template-columns: 20% 80%;  
+
+    @media (max-width:500px) {
+        grid-template-columns: 100%;
+        grid-template-rows: 50px;
+    }
+`
+export const FindItLogo = styled.div`
+    display: flex;
+    height: 100%;
+    width: 100%;
+    justify-content: center;
+    align-items: center;
+`
+export const GithubLogo = styled.div`
+    display: grid;
+    grid-template-columns: repeat(6, 33px);
+    grid-gap: 10px;
+    margin-right: 10px;
+    justify-self: flex-end;
+    align-items: center;
+    
 `

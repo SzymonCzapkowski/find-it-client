c1bcfd89d05ac3597d6f6a983afabfc6b2de527f
Unknown <magdalenakania1995@gmail.com>
2019-05-09 22:40:13 +0200

    Component ProjectFormMain

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,6 @@ import Header from './Layout/Header/Header';
 import TopNav from './Layout/TopNav/TopNav';
 import MainPage from './MainPage/MainPage';
 import ProfileView from './Profile/ProfileView/ProfileView';
-import ProjectForm from './Project/ProjectForm/ProjectForm';
 
 function App() {
   return (
@@ -17,7 +16,6 @@ function App() {
               <Route exact path="/" component={MainPage} />
               <Route exact path="/profile" component={ProfileView} />
               <Route path="/profile/:id" component={ProfileView} />
-              <Route path="/projectform" component={ProjectForm}/>
           </Switch>
       </Router>
       <Footer />

8f33fdc1209092d26d27df286c682b5a709fac88
Szymon Czapkowski <szymon.czapkowski@gmail.com>
2019-05-10 00:43:48 +0200

    TopNav edit

--- src/components/Layout/TopNav/TopNav.jsx
+++ src/components/Layout/TopNav/TopNav.jsx
@@ -1,24 +1,39 @@
-import React from 'react';
-import { TopNavContainer } from './TopNavStyles';
-import { HamburgerButton } from './TopNavStyles';
-
+import React, { useState, useEffect } from "react";
+import { TopNavContainer } from "./TopNavStyles";
+import { HamburgerButton } from "./TopNavStyles";
 
 const TopNav = () => {
+    const [isMenuDisplayed, setMenuDisplayed] = useState(false);
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
-        return (
-            <TopNavContainer>
-                <HamburgerButton>
-                </HamburgerButton>    
-                    <ul>
-                        <li>Kategoria 1</li>
-                        <li>Kategoria 2</li>
-                        <li>Kategoria 3</li>
-                        <li>Kategoria 4</li>
-                        <li>Kategoria 5</li>
-                    </ul>
-            </TopNavContainer>
-        );
+    useEffect(() => {
+        window.addEventListener("resize", updateWindowWidth);
+    }, []);
 
-}
+    const updateWindowWidth = () => {
+        setWindowWidth(window.innerWidth);
+    };
+
+    const handleClick = () => {
+        setMenuDisplayed(!isMenuDisplayed);
+    };
+
+    return (
+        <TopNavContainer>
+            {windowWidth <= 500 ? (
+                <HamburgerButton onClick={() => handleClick()} isMenuDisplayed={isMenuDisplayed}/>
+            ) : null}
+            {isMenuDisplayed || windowWidth > 500 ? (
+                <ul>
+                    <li>Kategoria 1</li>
+                    <li>Kategoria 2</li>
+                    <li>Kategoria 3</li>
+                    <li>Kategoria 4</li>
+                    <li>Kategoria 5</li>
+                </ul>
+            ) : null}
+        </TopNavContainer>
+    );
+};
 
 export default TopNav;
--- src/components/Layout/TopNav/TopNavStyles.js
+++ src/components/Layout/TopNav/TopNavStyles.js
@@ -1,71 +1,63 @@
 import styled from 'styled-components';
 
 export const TopNavContainer = styled.div`
-    display: flex;
-    justify-content:center;
-    font-size: 18px;
-    background: rgba(154, 176, 254, 0.8);
+   display: flex;
+   justify-content:center;
+   font-size: 18px;
+   background: rgba(154, 176, 254, 0.8);
 
-    ul {
-        display: flex;
-        list-style: none;
-        align-items: center; 
-        padding: 0;
-        justify-content: space-around;
-        height: 3vmin;
-        width: 60%;
-        box-sizing: border-box;
+   ul {
+       display: flex;
+       list-style: none;
+       align-items: center;
+       padding: 0;
+       justify-content: space-around;
+       height: 3vmin;
+       width: 60%;
+       box-sizing: border-box;
 
-        li {
-            color: white;  
-            margin: 0 1vh 0;
-            text-align: center;
-        }
+       li {
+           color: white;
+           margin: 0 1vw 0;
+           text-align: center;
 
-        li:hover {
-            background: rgba(154, 176, 254, 0.9);
-        }
+           :hover {
+               background: rgba(154, 176, 254, 0.9);
+           }
+       }
 
-       
-    @media (max-width:500px) {
-       display: grid; 
-       grid-template-rows: 5fr;
-       height: auto;
-       gap: 1vh;    
+   @media (max-width:500px) {
+      display: grid;
+      margin-top: 50px;
+      height: auto;
+      gap: 1vh;
     }
-    }
+   }
 `
 export const HamburgerButton = styled.button`
-    /* display: hidden; */
-    width: 40px;
-    height: 35px;
-    border: 0;
-    border-top: 5px solid white;
-    background: transparent;
-    position: relative;
-    transition: 0.3s transform linear;
-    transform: translateY(10px);
+   width: 40px;
+   height: 35px;
+   border: 0;
+   border-top: 5px solid black;
+   background: transparent;
+   position: absolute;
+   transition: 0.4s transform linear;
+   transform: translateY(10px);
 
-    ::before, ::after {
-    content: '';
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    border-top: 5px solid white;
-    transform: translateY(10px);
-    }
+   ::before, ::after {
+   content: '';
+   position: absolute;
+   top: 0;
+   left: 0;
+   width: 100%;
+   border-top: 5px solid black;
+   transform: translateY(10px);
+   }
 
-    ::after { 
-    transform: translateY(25px);
-    transition: 0.3s transform linear;
-    }
-   
-    :focus {
-    transform: rotate(90deg);
-    } 
-/* 
-    :focus, ::before, {
-    transform: translateY(-50px);
-    }   */
+   ::after {
+   transform: translateY(25px);
+   transition: 0.4s transform linear;
+   }
+
+   transform: ${props => props.isMenuDisplayed ? 'rotate(90deg) translateX(10px)' : ''}
 `

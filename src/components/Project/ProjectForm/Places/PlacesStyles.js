82d64d1e8dadea7be28fc07059c7b2cd77866def
Unknown <magdalenakania1995@gmail.com>
2019-05-12 19:06:58 +0200

    ProjectFormMain styles

--- src/components/Project/ProjectForm/Places/PlacesStyles.js
+++ src/components/Project/ProjectForm/Places/PlacesStyles.js
@@ -15,36 +15,11 @@ export const PlacesTitle = styled.p `
     margin: 0;
 `
 
-export const AddPlacesButton = styled.div `
-    display: flex;
-        justify-content: center;
-        align-items: center;
-    
-    div{
-        width: 38px;
-        height: 38px;
-        background-color: rgba(55, 88, 206, 1);
-        border-radius: 10px;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        font-family: Ubuntu;
-        font-style: normal;
-        font-weight: 300;
-        font-size: 40px;
-        color: white;
-    }
-
-    div:hover {
-        background-color: rgba(55, 88, 206, 0.8);
-        color: white;
-    }
-`
-
 export const PlacesContainer = styled.div `
     display: grid;
     grid-gap: 10px;
     grid-template-columns: repeat(3, 1fr);
+
 `
 
 export const Place = styled.div `
--- src/components/Project/ProjectForm/ProjectFormMain/ProjectFormMain.jsx
+++ src/components/Project/ProjectForm/ProjectFormMain/ProjectFormMain.jsx
@@ -3,6 +3,7 @@ import { withFormik, Field } from 'formik';
 import AddPlaceClass from '../AddPlace/AddPlace';
 import { Button } from 'react-bootstrap';
 import Places from '../Places/Places';
+import { Background, ProjectFormMainContainer, PlacesContainer, Section, Headerh1, Headerh3, FormContainer, ProjectName, ProjectNameLabel, ProjectNameInput, Description, DescriptionLabel, Category, CategoryLabel, PlusButton } from './ProjectFormMainStyles';
 
 
 class ProjectFormMainClass extends React.Component {
@@ -24,43 +25,93 @@ class ProjectFormMainClass extends React.Component {
 
 render() {
     return (
-        <form>
-        <h1> Create new project </h1> 
-        <h3>Create new project and find your best team!</h3>
-        <div>
-        <label> Project Name </label> 
-        <input name = "projectname" type = "text" />
-        </div> 
-        <div>
-           <label> Description </label> 
-           <Field
-           name = "messageText"
-           component = "textarea"
-           rows = "3" />
-        </div>
-        <div>
-        <label> Category </label>
-            <Field name = "category"
-            component = "select"
-            placeholder = "Category">
-                <option value = "javascript"> Java Script </option> 
-                <option value = "python" > Python </option> 
-                <option value = "java" > Java </option>
-                <option value = "PHP" > PHP </option>
-                <option value = "C#"> C# </option>
-                <option value = "C++"> C++ </option>
-                <option value = "Ruby"> Ruby </option>
-            </Field>
-        </div>
-       <div>
-       {this.state.isModalOpen ? 
-            <AddPlaceClass closeModal={this.closeModal} addPlace={this.addPlace}/>
-            : null
-        }
-            <Button onClick={() => this.setState({isModalOpen: true})}>+</Button>
-       </div>
-            <Places places={this.listElements}/>
-        </form>   
+        <Background>
+        <ProjectFormMainContainer>
+            <Headerh1>
+                Create new project
+            </Headerh1>
+
+            <Headerh3>
+                Create new project and find your best team!
+            </Headerh3>
+
+            <FormContainer>
+                <ProjectName>
+                    <ProjectNameLabel > Project Name </ProjectNameLabel>
+                    <ProjectNameInput style = {{
+                        "border-radius": "6%",
+                        "width": "60%"
+                    }}
+                    name = "projectname" type = "text"/> 
+                </ProjectName>
+
+
+                <Description>
+                    <DescriptionLabel>
+                        Description 
+                    </DescriptionLabel>
+
+                    <Field style = {
+                        {
+                            "border-radius": "6%",
+                            "width": "60%"                 
+                        }
+                    }
+                    name = "messageText"
+                    component = "textarea"
+                    rows = "6" />
+                </Description>
+                <Section>
+                    <Category>  
+                        <CategoryLabel> Category </CategoryLabel>
+                        <Field style = {
+                            {
+                                "display": "inline-block",
+                                "margin-top": "1%",
+                                "font-size": "18px"
+                            }
+                        }
+                
+                        name = "category"
+                        component = "select"
+                        placeholder = "Category">
+                            <option value = "javascript"> Java Script </option> 
+                            <option value = "python" > Python </option> 
+                            <option value = "java" > Java </option>
+                            <option value = "PHP" > PHP </option>
+                            <option value = "C#"> C# </option>
+                            <option value = "C++"> C++ </option>
+                            <option value = "Ruby"> Ruby </option>
+                        </Field>
+                    </Category>
+                    
+                    <PlusButton>
+                        {this.state.isModalOpen ? 
+                                <AddPlaceClass closeModal={this.closeModal} addPlace={this.addPlace}/>
+                                : null
+                            }
+                                <Button style = {{
+                                        "background-color": "rgba(55, 88, 206, 0.8)",
+                                        "font-size": "30px",
+                                        "width": "100%",
+                                        "border-radius": "30%",
+                                        "border-style": "none"
+
+                                }}
+                                
+                                
+                                
+                                onClick={() => this.setState({isModalOpen: true})}>+</Button>
+                        
+                    </PlusButton>
+                    <PlacesContainer>
+                    <Places
+                    places={this.listElements}/>  
+                    </PlacesContainer>
+                </Section>
+            </FormContainer>
+        </ProjectFormMainContainer>
+        </Background>
     );
 }
 }

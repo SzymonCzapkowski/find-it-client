fc2cf4567e5c0160f050229b33a7e4cce46521b3
Adam Oleszko <adamoleszko44@gmail.com>
2019-05-15 23:46:17 +0200

    Updates

---
+++ precache-manifest.b1f8b5194161da18e6364a7cfa080682.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9be45b825f5577ced020ca5da81dd666",
+    "url": "/find-it-client/index.html"
+  },
+  {
+    "revision": "9e2b053a02bcadb65252",
+    "url": "/find-it-client/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "aba69cdbe7b86cf8635e",
+    "url": "/find-it-client/static/js/2.fc97d843.chunk.js"
+  },
+  {
+    "revision": "9e2b053a02bcadb65252",
+    "url": "/find-it-client/static/js/main.2df0bd0b.chunk.js"
+  },
+  {
+    "revision": "6a0e82ccc7fdb54e4709",
+    "url": "/find-it-client/static/js/runtime~main.89cc97d1.js"
+  },
+  {
+    "revision": "8e409c4a99567ee433527b76fb815633",
+    "url": "/find-it-client/static/media/bg_black.8e409c4a.jpg"
+  },
+  {
+    "revision": "9c6cd67b809eb64b304bea78a2f8fd89",
+    "url": "/find-it-client/static/media/bg_blue.9c6cd67b.jpg"
+  },
+  {
+    "revision": "c59786de2456cb7c3212a9795880ef83",
+    "url": "/find-it-client/static/media/photo.c59786de.jpg"
+  }
+]);
